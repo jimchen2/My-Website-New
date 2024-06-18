30c98f46950aef3c487da736c721b5ee42109759
Jiamu Chen <jimchen4214@gmail.com>
2024-06-19 05:15:09 +0800

    updates

--- setup/setup-ubuntu-22.sh
+++ setup/setup-ubuntu-22.sh
@@ -19,7 +19,7 @@ sudo systemctl enable --now mongod
 
 sudo mkdir -p /etc/nginx/{sites-available,sites-enabled}
 sudo systemctl enable --now nginx
-sudo cp /var/www/My-Website/setup/nginx.conf /etc/nginx/nginx.conf
+# sudo cp /var/www/My-Website/setup/nginx.conf /etc/nginx/nginx.conf
 
 ##################################################################
 
@@ -46,14 +46,16 @@ sudo systemctl enable update-mywebsite.timer
 
 
 sudo git clone https://github.com/jimchen2/My-Website-New /var/www/My-Website
-sudo chown -R ubuntu:ubuntu /var/www/My-Website-zh
+sudo chown -R ubuntu:ubuntu /var/www/My-Website
 sudo certbot certonly --standalone -d cn.jimchen.me --email jimchen4214@gmail.com --non-interactive --agree-tos
+sudo certbot certonly --standalone -d jimchen.me --email jimchen4214@gmail.com
 sudo ln -sf /etc/nginx/sites-available/cn.jimchen.me.conf /etc/nginx/sites-enabled/
 sudo cp /var/www/My-Website/setup/cn.jimchen.me.conf /etc/nginx/sites-available/cn.jimchen.me.conf
+sudo ln -sf /etc/nginx/sites-available/jimchen.me.conf /etc/nginx/sites-enabled/
+sudo cp /var/www/My-Website/setup/jimchen.me.conf /etc/nginx/sites-available/jimchen.me.conf
 
 
-
-sudo cp /var/www/My-Website/setup/{my-website.service,update-mywebsite.service,update-mywebsite.timer} /etc/systemd/system/
-sudo systemctl start update-mywebsite.service
+sudo cp /var/www/My-Website/setup/{my-website.service,update-mywebsite-zh.service,update-mywebsite-zh.timer} /etc/systemd/system/
+sudo systemctl start update-mywebsite-zh.service
 sudo systemctl enable --now my-website.service
-sudo systemctl enable update-mywebsite.timer
+sudo systemctl enable update-mywebsite-zh.timer
