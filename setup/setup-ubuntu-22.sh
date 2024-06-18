fd0811722f6e704ee2d4054666e9f15d18093db4
Jiamu Chen <jimchen4214@gmail.com>
2024-06-19 02:18:57 +0800

    updates

--- setup/setup-ubuntu-22.sh
+++ setup/setup-ubuntu-22.sh
@@ -30,12 +30,15 @@ sudo chown -R ubuntu:ubuntu /var/www/My-Website-zh
 
 # sudo systemctl stop ufw
 sudo certbot certonly --standalone -d jimchen.me --email jimchen4214@gmail.com --non-interactive --agree-tos
-   sudo certbot certonly --standalone -d cn.jimchen.me --email jimchen4214@gmail.com --non-interactive --agree-tos
+sudo certbot certonly --standalone -d cn.jimchen.me --email jimchen4214@gmail.com --non-interactive --agree-tos
+sudo certbot certonly --standalone -d www.jimchen.me --email jimchen4214@gmail.com --non-interactive --agree-tos
 
-# sudo ln -sf /etc/nginx/sites-available/jimchen.me.conf /etc/nginx/sites-enabled/
-# sudo ln -sf /etc/nginx/sites-available/cn.jimchen.me.conf /etc/nginx/sites-enabled/
-# sudo cp /var/www/My-Website/setup/jimchen.me.conf /etc/nginx/sites-available/jimchen.me.conf
-# sudo cp /var/www/My-Website/setup/cn.jimchen.me.conf /etc/nginx/sites-available/cn.jimchen.me.conf
+sudo ln -sf /etc/nginx/sites-available/jimchen.me.conf /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/cn.jimchen.me.conf /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/www.jimchen.me.conf /etc/nginx/sites-enabled/
+sudo cp /var/www/My-Website/setup/jimchen.me.conf /etc/nginx/sites-available/jimchen.me.conf
+sudo cp /var/www/My-Website/setup/cn.jimchen.me.conf /etc/nginx/sites-available/cn.jimchen.me.conf
+sudo cp /var/www/My-Website/setup/www.jimchen.me.conf /etc/nginx/sites-available/www.jimchen.me.conf
 
 sudo cp /var/www/My-Website/setup/{my-website.service,update-mywebsite.service,update-mywebsite.timer} /etc/systemd/system/
 sudo systemctl start update-mywebsite.service
