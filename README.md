cb8bf4e3a949568f8deb0a8620599ea8121709eb
Jim Chen <jimchen4214@gmail.com>
2025-03-18 06:42:58 +0800

    Updates

---
+++ README.md
@@ -0,0 +1,5 @@
+Deployed on Vercel
+
+Migrated to Next.js from react-router-dom
+
+React Bootstrap
--- src/static/footer.js
+++ src/static/footer.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { useGlobalColorScheme } from "../config/global.js";
 import Image from "next/image"; // Add this import
+import Link from 'next/link';
 
 const ICON_SIZE = 35;
 
@@ -44,29 +45,22 @@ function Footer() {
   };
 
   const externalLinkIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="16"
-      height="16"
-      fill="currentColor"
-      style={{ verticalAlign: "middle", marginLeft: "0px" }}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor" style={{ verticalAlign: "middle", marginLeft: "0px" }}>
       <path d="M14 3h7v7h-2V6.41L10.41 15 9 13.59 17.59 5H14V3zM5 5h4v2H5v12h12v-4h2v4c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2V7c0-1.1.9-2 2-2z" />
     </svg>
   );
 
   const CopyrightSection = ({ year, linkStyle }) => (
     <div>
-      <span style={{ color: colors.color_black }}>Copyright © Jim Chen {year}</span>,
-      <span style={{ margin: "2px" }}></span>
+      <span style={{ color: colors.color_black }}>Copyright © Jim Chen {year}</span>,<span style={{ margin: "2px" }}></span>
       <a href="https://github.com/jimchen2/My-Website" style={linkStyle}>
         Source{externalLinkIcon}
-      </a>,
-      <span style={{ margin: "2px" }}></span>
-      <a href="${process.env.NEXT_PUBLIC_SITE}/api/rss" style={linkStyle}>
-        RSS{externalLinkIcon}
       </a>
+      ,<span style={{ margin: "2px" }}></span>
+      <Link href={`${process.env.NEXT_PUBLIC_SITE}/api/rss`} style={linkStyle}>
+        {" "}
+        RSS{externalLinkIcon}
+      </Link>
     </div>
   );
 
@@ -84,13 +78,7 @@ const IconLinks = ({ imageStyle, linkStyle }) => (
   <div className="justify-content-end">
     {socialLinks.map((link) => (
       <a key={link.href} href={link.href} style={linkStyle}>
-        <Image
-          src={link.imgSrc}
-          alt={link.alt}
-          width={ICON_SIZE}
-          height={ICON_SIZE}
-          style={imageStyle}
-        />
+        <Image src={link.imgSrc} alt={link.alt} width={ICON_SIZE} height={ICON_SIZE} style={imageStyle} />
       </a>
     ))}
   </div>
